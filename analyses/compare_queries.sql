719775cac74ea5004e3c332562a57783313fcbd9
fadi.interworks@gmail.com <fadi.interworks@gmail.com>
2023-01-16 18:26:19 +0000

    compare

--- analyses/compare_queries.sql
+++ analyses/compare_queries.sql
@@ -1,8 +1,6 @@
 {% set old_etl_relation=ref('customer_orders') %}
 {% set dbt_relation=ref('fct_customer_orders') %}
 
-
-
 {{ audit_helper.compare_relations(
     a_relation=old_etl_relation,
     b_relation=dbt_relation,
