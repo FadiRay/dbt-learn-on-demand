9ecb680a9f0b2f86612ec655d8aed2711ff94d32
fadi.interworks@gmail.com <fadi.interworks@gmail.com>
2023-01-17 21:27:48 +0000

    int_orders__pivoted

---
+++ models/marts/init_orders__pivoted.sql
@@ -0,0 +1,21 @@
+{% set payment_methods = ['bank_transfer', 'credit_card', 'coupon', 'gift_card'] -%}
+
+with
+payments as (
+   select * from {{ ref('stg_jaffle_shop__customers') }}
+),
+
+final as (
+    order_id,
+    {% for payment_method in payment_methods -%}
+    sum(case {{payment_method}} then amount else 0 end) as {{payment_method}}_amount
+    {%- if not loop.last -%}
+    ,
+    {% endif -%}
+    {% endfor -%}
+
+)
+
+select * from final
+
+
